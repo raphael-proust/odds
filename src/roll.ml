36fc2f9885470596cf62d0f3ba5c66c887284c88
Raphaël Proust <code@bnwr.net>
2017-07-12 21:22:12 +0100

    Add optional seed argument to CLI
    Fixes #4

--- src/roll.ml
+++ src/roll.ml
@@ -1,21 +1,28 @@
 
+let seed = Cmdliner.Arg.(
+	value & opt (some int) None & info ["s"; "seed"] ~doc:"The seed for the PRNG" ~docv:"SEED"
+)
+
 let formula = Cmdliner.Arg.(
 	value & pos 0 string "0" & info [] ~doc:"The dice formula to roll" ~docv:"FORMULA"
 )
 
-let main formula =
+let main (seed: int option) formula =
 
-	Random.self_init ();
+	let state = match seed with
+		| None -> None
+		| Some seed -> Some (Random.State.make [| seed |])
+	in
 
 	let t = Odds.t_of_string formula in
-	let r = Odds.roll t in
+	let r = Odds.roll ?state t in
 	print_int r;
 	print_newline ();
 	flush_all ()
 
 
 let main_t = Cmdliner.Term.(
-	pure main $ formula
+	pure main $ seed $ formula
 )
 
 let info =
