913acb453f2aab703edec39e7fb75055f229e45b
Raphaël Proust <code@bnwr.net>
2022-06-10 09:28:48 +0200

    roller test: check for returned value as well

--- tests/roller.ml
+++ tests/roller.ml
@@ -2,11 +2,12 @@ let rolls_of_formula formula =
 	let rolls = ref [] in
 	Effect.Deep.match_with
 		Odds.Dice.eval formula
-		{ Effect.Deep.retc = (fun _ -> Ok !rolls);
+		{ Effect.Deep.retc = (fun r -> Ok (r, !rolls));
 			exnc = (fun e -> Error e);
 			effc = (fun (type a) (type b) (e : a Effect.t) : ((a, b) Effect.Deep.continuation -> b) option ->
 				match e with
 				| Odds.Dice.Roll faces -> Some (fun k ->
+					(* For testing: log effect, return max result *)
 					rolls := faces :: !rolls;
 					Effect.Deep.continue k faces
 				)
@@ -16,18 +17,17 @@ let rolls_of_formula formula =
 
 let check_rolls_of_formula f check =
 	let rolls = rolls_of_formula f in
-	let rolls = Result.map List.rev rolls in
 	check rolls
 
 let () =
 	Printf.printf "Checking roller%!";
-	check_rolls_of_formula Odds.Dice.(Dice (1, 10)) (function Ok [10] -> () | _ -> assert false);
+	assert (check_rolls_of_formula Odds.Dice.(Dice (1, 10)) (fun v -> v = Ok (10, [10])));
 	Printf.printf ".%!";
-	check_rolls_of_formula Odds.Dice.(Plus (Dice (1,10), Dice (1,20))) (function Ok [10;20] -> () | _ -> assert false);
+	assert (check_rolls_of_formula Odds.Dice.(Minus (Dice (1,10), Dice (1,20))) (fun v -> v = Ok ((-10), [20;10])));
 	Printf.printf ".%!";
-	check_rolls_of_formula (Odds.Dice.Constant 0) (function Ok [] -> () | _ -> assert false);
+	assert (check_rolls_of_formula (Odds.Dice.Constant 0) (fun v -> v = Ok (0, [])));
 	Printf.printf ".%!";
-	check_rolls_of_formula Odds.Dice.(Dice (1,0)) (function Error _ -> () | _ -> assert false);
+	assert (check_rolls_of_formula Odds.Dice.(Dice (1,0)) Result.is_error);
 	Printf.printf ".%!";
 	Printf.printf "\nDone%!";
 	exit 0;
